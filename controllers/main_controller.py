f07f2d70ef9d7ee87e7c8b36627d6646f8d2a8b1
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-09-19 10:58:38 +0200

    MainController class: Book management

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -37,10 +37,61 @@ class MainController:
             else:
                 print("Invalid choice 🤔. Please try again!")
 
+    # --- user management ---
     def manage_users(self):
         pass
 
+    # --- Book management ---
+    def manage_books(self):
+        while True:
+            print("\n--- Main Books ---")
+            print("1. Add a book")
+            print("2. Delete a book")
+            print("3. Search for book")
+            print("4. Borrow a book")
+            print("5. Return a book")
+            print("6. List books")
+            print("7. Back to main menu")
 
+            choice = input("Enter your choice 👉: ")
+            if choice == "1":
+                self.add_book()
+            elif choice == "2":
+                self.delete_book()
+            elif choice == "3":
+                self.search_book()
+            elif choice == "4":
+                self.borrow_book()
+            elif choice == "5":
+                self.return_book()
+            elif choice == "6":
+                self.get_list_book()
+            elif choice == "7":
+                return
+            print("Invalid choice 🤔. Please try again!")
+
+    def add_book(self):
+        pass
+
+    def delete_book(self):
+        pass
+
+    def add_books(self):
+        pass
+
+    def search_book(self):
+        pass
+
+    def borrow_book(self):
+        pass
+
+    def return_book(self):
+        pass
+
+    def get_list_book(self):
+        pass
+
+    # --- Statistics management ---
     def generate_statistics(self):
         pass
 
