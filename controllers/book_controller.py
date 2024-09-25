0637d24324391cf10ef6127b79f8fb635ec60846
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-09-25 13:07:08 +0200

    feat(main_controller): Add find_book_by_isbn function

--- controllers/book_controller.py
+++ controllers/book_controller.py
@@ -6,7 +6,7 @@ class BookController:
     def __init__(self):
         self.books = load_books()
 
-    def add_user(self, title, author, isbn, summary):
+    def add_book(self, title, author, isbn, summary):
         book = Book(title, author, isbn, summary)
         # ... Générer le cas ou le book existe déjà
         self.books.append(book)
@@ -14,3 +14,9 @@ class BookController:
 
     def list_books(self):
         return self.books
+
+    def find_book_by_isbn(self, isbn):
+        for book in self.books:
+            if book.isbn == isbn:
+                return book
+        return
