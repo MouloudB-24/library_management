e8d12364bdd6e78538810ea0641e19c9594c7854
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-09-28 06:58:01 +0200

    feat(user_controller):Update add_user method

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -7,23 +7,7 @@ class UserController:
         self.users = load_users()
 
     def add_user(self, name, membership_no):
-        user = User(name, membership_no)
-        # ... Générer le cas ou le user existe déjà
-        self.users.append(user)
-        save_users(self.users)
-
-    def delete_user(self, membership_no):
-        user = self.find_user_by_membership_no(membership_no)
-        if user:
-            self.users.remove()
-        return
-
-    def find_user_by_membership_no(self, membership_no):
-        for user in self.users:
-            if user.membership_no == membership_no:
-                return user
-        return
-
+        return User(name, membership_no)
 
     def list_users(self):
         return self.users
