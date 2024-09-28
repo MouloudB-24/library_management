9475f3ffb192c486693ace25fd7faefef295e13c
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-09-28 06:37:55 +0200

    feat(main_controller): Update options menu & create_library & add_user, add_book, delete_user, delete_book methods

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -40,86 +40,118 @@ class MainController:
 
     # --- Create library ---
     def manage_library(self):
-        options = ["a. Create library", "b. Add user", "c. Add book", "d. Delete user", "e. Delete book",
-                   "f. Update user", "g. Update book", "h. Search user", "i. Search book", "j. Borrow book",
-                   "k. Return book", "l. List users", "m. List books", "n. Back to main menu"]
+        # Création de la bibliothèque
+        self.create_library()
+
+        options = ['Manage menu library 👇:', '1. Add user', '2. Add book', '3. Delete user', '4. Delete book', '5. Update user',
+                   '6. Update book', '7. Search user', '8. Search book', '9. Borrow book', '10. Return book',
+                   '11. List users', '12. List books', '13. Back to main menu']
+
         while True:
             for option in options:
                 print(option)
 
             choice = self.get_user_choice()
 
-            if choice.lower() == "a":
-                self.create_library()
-            elif choice.lower() == "b":
+            if choice.lower() == "1":
                 self.add_user()
-            elif choice.lower() == "c":
+            elif choice.lower() == "2":
                 self.add_book()
-            elif choice.lower() == "d":
+            elif choice.lower() == "3":
                 self.delete_user()
-            elif choice.lower() == "e":
+            elif choice.lower() == "4":
                 self.delete_book()
-            elif choice.lower() == "f":
+            elif choice.lower() == "5":
                 self.update_user()
-            elif choice.lower() == "g":
+            elif choice.lower() == "6":
                 self.update_book()
-            elif choice.lower() == "h":
+            elif choice.lower() == "7":
                 self.search_user()
-            elif choice.lower() == "i":
+            elif choice.lower() == "8":
                 self.search_book()
-            elif choice.lower() == "j":
+            elif choice.lower() == "9":
                 self.borrow_book()
-            elif choice.lower() == "k":
+            elif choice.lower() == "10":
                 self.return_book()
-            elif choice.lower() == "l":
+            elif choice.lower() == "11":
                 self.get_list_users()
-            elif choice.lower() == "m":
+            elif choice.lower() == "12":
                 self.get_list_book()
-            elif choice.lower() == "n":
+            elif choice.lower() == "13":
                 return
             else:
                 print("Invalid choice 🤔. Please try again!")
 
     def create_library(self):
         try:
+            if self.library_controller.library:
+                return
             name, address = self.library_view.get_library_details()
             self.library_controller.create_library(name, address)
-            print("Library created successfully 👏")
+            print("Library successfully updated  👏")
 
         except Exception as e:
-            print(f"Error creating library: {e}")
+            print(f"Error create library: {e}")
 
     def add_user(self):
         try:
             name, membership_no = self.user_view.get_user_details()
             # ... Ajouter des fonctions de validate de doonées !!
 
-            self.user_controller.add_user(name, membership_no)
+            # Créer le user
+            user = self.user_controller.add_user(name, membership_no)
+            # Ajouter le user
+            self.library_controller.add_user_to_library(user)
             print("The user is successfully added 👏")
 
         except Exception as e:
-            print(f"Error adding user ‼️: {e}")
+            print(f"Error adding user: {e}")
+
+    def add_book(self):
+        try:
+            title, author, isbn, summary = self.book_view.get_book_details()
+            # Ajouter des fonction de validation ...
+
+            book = self.book_controller.add_book(title, author, isbn, summary)
+            self.library_controller.add_book_to_library(book)
+            print("The book is successfully added 👏")
+        except Exception as e:
+            print(f"Error adding book: {e}")
 
     def delete_user(self):
         try:
             membership_no = self.user_view.get_user_membership_no()
-            self.user_controller.delete_user(membership_no)
-            print("The user is successfully deleted 👏")
+            user = self.library_controller.find_user_by_membership_no(membership_no)
+            if user:
+                self.library_controller.delete_user_from_library(user)
+                print("The user is successfully deleted 👏")
+            else:
+                print("The user not found!")
+
         except Exception as e:
-            print(f"Error suppression user ‼️: {e}")
+            print(f"Error suppression user: {e}")
+
+    def delete_book(self):
+        try:
+            isbn = self.book_view.get_isbn_book()
+            book = self.library_controller.find_book_by_isbn(isbn)
+            if book:
+                self.library_controller.delete_book_from_library(book)
+                print("The book is successfully deleted 👏")
+            else:
+                print("The book not found!")
+
+        except Exception as e:
+            print(f"Error suppression book: {e}")
 
     def update_user(self):
         pass
 
-    def get_list_users(self):
+    def update_book(self):
         pass
 
-    def add_book(self):
-        title, author, isbn, summary = self.book_view.get_book_details()
-        # Ajouter des fonction de validation ...
-
-        self.book_controller.add_book(title, author, isbn, summary)
-        print("The book is successfully added 👏")
+    def get_list_users(self):
+        pass
 
     def search_book(self):
         isbn = self.book_view.get_isbn_book()
@@ -129,14 +161,8 @@ class MainController:
         else:
             print("Book not found 🤗")
 
-    def delete_book(self):
-        isbn = self.book_view.get_isbn_book()
-        book = self.book_controller.find_book_by_isbn(isbn)
-        if book:
-            self.book_controller.delete_book(book)
-            print("The book is successfully deleted 👏")
-        else:
-            print("Book not found 🤗")
+    def search_user(self):
+        pass
 
     def borrow_book(self):
         pass
