e9d533b1eae0c0aabf55bbf56ab7b71c56813101
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-09-30 14:13:05 +0200

    Delete save user, save book, load user and load book functions

--- others/data_management.py
+++ others/data_management.py
@@ -1,7 +1,5 @@
 import json
 from pathlib import Path
-from models.user import User
-from models.book import Book
 from models.library import Library
 
 
@@ -13,46 +11,6 @@ def create_backup_folder():
 
 
 BACKUP_FOLDER = create_backup_folder()
-
-
-def save_users(users):
-    try:
-        with open(BACKUP_FOLDER / "users.json", "w", encoding="utf-8") as file:
-            json.dump([user.to_dict() for user in users], file, indent=4)
-    except Exception as e:
-        print(f"Error saving users: {e}")
-
-
-def load_users():
-    try:
-        with open(BACKUP_FOLDER / "users.json", "r", encoding="utf-8") as file:
-            users = json.load(file)
-            return [User.from_dict(user) for user in users]
-    except FileNotFoundError:
-        return []
-    except json.JSONDecodeError:
-        print("JSON decoding error when loading users")
-        return []
-
-
-def save_books(books):
-    try:
-        with open(BACKUP_FOLDER / "books.json", "w", encoding="utf-8") as file:
-            json.dump([book.to_dict() for book in books], file, indent=4)
-    except Exception as e:
-        print(f"Error saving books: {e}")
-
-
-def load_books():
-    try:
-        with open(BACKUP_FOLDER / "books.json", "r", encoding="utf-8") as file:
-            books = json.load(file)
-            return [Book.from_dict(book) for book in books]
-    except FileNotFoundError:
-        return []
-    except json.JSONDecodeError:
-        print("JSON decoding error when loading books")
-        return []
 
 
 def save_library(library):
@@ -67,12 +25,12 @@ def load_library():
     try:
         with open(BACKUP_FOLDER / "library.json", "r", encoding="utf-8") as file:
             library = json.load(file)
-            return [Library.from_dict(library)]
+            return Library.from_dict(library)
     except FileNotFoundError:
-        return []
+        return {}
     except json.JSONDecodeError:
         print("JSON decoding error when loading library")
-        return []
+        return {}
 
 
 if __name__ == "__main__":
